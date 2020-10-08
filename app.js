8e2675d490a389c49d0329fe7dd3b9e8883a1977
ashish0013shaw <ashish0013shaw@gmail.com>
2020-10-08 20:37:09 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+/*eslint-env es6*/
+const hamburger=document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu=document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item=document.querySelectorAll('.header .nav-bar .nav-list ul a');
+const header=document.querySelector('.header .container');
+
+hamburger.addEventListener('click',()=>{
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+document.addEventListener('scroll',()=>{
+	var scroll_position = window.scrollY;
+   if(scroll_position >250){
+	document.getElementById('header').style.backgroundColor='#29323C';
+   }else{
+	document.getElementById('header').style.backgroundColor='transparent';
+    }
+});
+menu_item.forEach(item=>{
+	item.addEventListener('click',()=>{
+		hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+	});
+});
